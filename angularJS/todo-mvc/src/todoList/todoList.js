e83f162e94870958ccf3d3d3af952b615166650b
frankxin <frankxin93@gmail.com>
2015-10-06 22:49:01 +0800

    angular todo

--- angularJS/todo-mvc/src/todoList/todoList.js
+++ angularJS/todo-mvc/src/todoList/todoList.js
@@ -2,6 +2,7 @@ var todoApp = angular.module('todoApp',[]);
 
 todoApp.controller('todoListControler',function(){
 
+  //model
   this.todos = [{
     text: "test task1",
     done: false
@@ -15,6 +16,29 @@ todoApp.controller('todoListControler',function(){
 
     this.todos.push({text:this.newTodoText, done:false});
     this.newTodoText = "";
+
+  }
+
+  this.remaining = function(){
+
+    var count = 0;
+    angular.forEach(this.todos , function(todo){
+      count += todo.done ? 0 : 1;
+    })
+    return count;
+
+  }
+
+  this.archive = function(){
+    
+    var newTodos = [],
+        todos = this.todos
+
+    angular.forEach(this.todos , function(todo,key){
+      if(!todo.done){newTodos.push(todo)}
+    })
+
+    this.todos = newTodos;
 
   }
   console.log(this)
